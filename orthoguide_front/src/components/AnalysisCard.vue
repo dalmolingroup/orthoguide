a78b8c18a466c6134c70c9bfa2a4fed67df8e3ec
jvfe <joao.cavalcante.073@ufrn.edu.br>
2025-10-13 11:12:17 -0300

    chore: Update changelog and version statement

--- orthoguide_front/src/components/AnalysisCard.vue
+++ orthoguide_front/src/components/AnalysisCard.vue
@@ -125,7 +125,7 @@
         </button>
       </div>
     </div>
-    <span id="version-statement">OrthoGuide v2.10.0</span>
+    <span id="version-statement">OrthoGuide v2.11.0</span>
   </main>
 </template>
 
